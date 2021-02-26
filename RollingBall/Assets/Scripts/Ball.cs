72554286e4bd545d23f27655a4233a280299412d
solomka121 <slavasolomahin12@gmail.com>
2021-02-26 13:44:18 +0200

    Base

---
+++ RollingBall/Assets/Scripts/Ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+internal class RollingBall
+{
+    private float _speed;
+    public float speed
+    {
+        get => _speed;
+        set
+        {
+            _speed = value;
+        }
+    }
+}
